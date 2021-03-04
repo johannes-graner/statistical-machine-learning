49e716ee76ba19f037be85f58504654c4d5c4c44
JohannesGraner <johannes.graner@gmail.com>
2021-03-04 17:01:49 +0100

    chromium set to run in background

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 #docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/scipy-notebook
 JUPYTER_TOKEN=my_token
 docker run --rm --gpus all -d -it -p 8848:8888 -v $PWD:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e JUPYTER_TOKEN=$JUPYTER_TOKEN --user root cschranz/gpu-jupyter:v1.3_cuda-10.1_ubuntu-18.04_python-only
-chromium http://localhost:8848?token=${JUPYTER_TOKEN}
+chromium http://localhost:8848?token=${JUPYTER_TOKEN} &
